7b9e53010bc5f33ea487364179a00eb7168a5956
pmcrockett <102689325+pmcrockett@users.noreply.github.com>
2023-12-04 19:16:06 -0800

    Handle all errors and add keyboard input

--- calculator.js
+++ calculator.js
@@ -1,28 +1,64 @@
 let g_display = document.querySelector(".display");
-let g_history = document.querySelector(".history");
+let g_eq = document.querySelector(".eq");
 let g_opButtons = document.querySelectorAll(".op");
 let g_clearButton = document.querySelector(".clear");
-let g_backButton = document.querySelector(".back");
+let g_undoButton = document.querySelector(".undo");
 let g_displayStr = "";
+let g_blockInput = false;
 
 initListeners();
 
-// Initializes buttons
+// Initializes input events
 function initListeners() {
     g_opButtons.forEach(_elem => {
-        _elem.addEventListener("click", function () {
-            console.log(_elem.textContent);
-            g_displayStr += _elem.textContent;
+        _elem.addEventListener("click", function() {
+            if (!g_blockInput) {
+                g_displayStr += _elem.textContent;
+            }
             updateDisplay();
         });
     });
-    g_clearButton.addEventListener("click", function () {
+
+    g_clearButton.addEventListener("click", function() {
         g_displayStr = "";
+        g_blockInput = false;
         updateDisplay();
     });
-    g_backButton.addEventListener("click", function () {
+
+    g_undoButton.addEventListener("click", function () {
         g_displayStr = g_displayStr.slice(0, g_displayStr.length - 1);
+        g_blockInput = false;
         updateDisplay();
+    });
+
+    document.addEventListener("keydown", function(_e) {
+        let selector = ""
+
+        if (_e.key >= "0" && _e.key <= "9") {
+            selector = _e.key;
+        } else if (_e.key == "+") {
+            selector = "add";
+        } else if (_e.key == "-") {
+            selector = "sub";
+        } else if (_e.key == "*") {
+            selector = "mult";
+        } else if (_e.key == "\/") {
+            selector = "div";
+        } else if (_e.key == ".") {
+            selector = "dot";
+        } else if (_e.key == "Enter") {
+            selector = "enter";
+        } else if (_e.key == "Escape") {
+            selector = "clear";
+        } else if (_e.key == "Backspace") {
+            selector = "undo";
+        }
+
+        if (selector.length) {
+            let event = new Event("click");
+            let button = document.querySelector(`.key-${selector}`);
+            button.dispatchEvent(event);
+        }
     });
 }
 
@@ -30,7 +66,11 @@ function initListeners() {
 function updateDisplay() {
     let input = parseInput(g_displayStr);
     g_displayStr = input.str;
-    g_history.textContent = getDisplayEq(input);
+    g_eq.textContent = getDisplayEq(input);
+
+    if (g_eq.textContent == "Div0") {
+        g_eq.textContent = ":(";
+    }
     
     if (input.num2 && isNum(input.num2)) {
         let newText = operate(Number(input.num1), Number(input.num2), input.opStr);
@@ -38,13 +78,18 @@ function updateDisplay() {
         if (newText != "Div0") {
             g_display.textContent = newText;
         } else {
-            g_display.textContent = "Division by zero :(";
-            //g_displayStr = g_displayStr.slice(0, g_displayStr.length - 1);
+            g_display.textContent = "";
         }
 
         g_display.classList.add("disable");
-    } else if (isNum(input.num1)) {
-        g_display.textContent = input.num1;
+    } else if (isNum(input.num1) && input.opStr == "") {
+        if (input.num1 == "Div0") {
+            g_blockInput = true;
+            g_display.textContent = "Division by zero :(";
+        } else {
+            g_display.textContent = input.num1;
+        }
+
         g_display.classList.remove("disable");
     } else {
         g_display.textContent = "";
@@ -83,7 +128,6 @@ function parseInput(_str) {
             if (nums.length >= 2 && opStr.length && nums[0].length && isNum(nums[1])) {
                 runOp = true;
             } else {
-                console.log("Error (equals)");
                 _str = _str.slice(0, _str.length - 1);
             }
         } else if (_str[i] >= "0" && _str[i] <= "9") {
@@ -96,10 +140,8 @@ function parseInput(_str) {
         } else if (_str[i] == "+" || _str[i] == "-" || _str[i] == "*" 
                 || _str[i] == "\/") {
             if (nums.length >= 2 && isNum(nums[1])) {
-                if (!(_str[i] == "\/" && Number(nums[1]) == 0)) {
-                    runOp = true;
-                    i--;
-                }
+                runOp = true;
+                i--;
             } else if (opStr == "" && isNum(nums[0])) {
                 opStr = _str[i];
                 nums.push("");
@@ -107,25 +149,25 @@ function parseInput(_str) {
             } else if (_str[i] == "-" && !nums[nums.length - 1].length) {
                 nums[nums.length - 1] += "-";
             } else if (_str[i] != "+") {
-                console.log("Error (opStr != +)");
                 _str = _str.slice(0, _str.length - 1);
-                //break;
             }
         } else {
-            // Error
-            console.log("Error");
             _str = _str.slice(0, _str.length - 1);
             break;
         }
 
         if (nums.length >= 2 && opStr.length && isNum(nums[0]) && isNum(nums[1])
                 && runOp) {
-            //if (!(opStr = "\/" && Number(nums[1]) == 0)) {
-                nums[0] = operate(Number(nums[0]), Number(nums[1]), opStr);
-                nums.pop();
+            nums[0] = operate(Number(nums[0]), Number(nums[1]), opStr);
+
+            if (nums[0] == "Div0") {
+                nums[1] = "";
                 opStr = "";
-            //}
+                break;
+            }
             
+            nums.pop();
+            opStr = "";
             runOp = false;
         }
     }
@@ -133,9 +175,11 @@ function parseInput(_str) {
     return { num1: nums[0], num2: nums[1] || null, opStr, str: _str };
 }
 
-// Checks if a number string has numbers or just a negative symbol.
+// Checks if a number string has content or just a negative/dot symbol.
 function isNum(_str) {
-    return _str.length > 1 || (_str.length && _str[0] != "-" && _str[0] != ".");
+    return _str.length > 2 
+        || (_str.length == 1 && _str[0] != "-" && _str[0] != ".")
+        || (_str.length == 2 && (_str[0] != "-" || _str[1] != "."));
 }
 
 // Does the math.
@@ -159,6 +203,7 @@ function operate(_num1, _num2, _opStr) {
     return result;
 }
 
+// Converts number to formatted string.
 function formatFloat(_num) {
     return trimZeroes(String(_num.toFixed(6)));
 }
